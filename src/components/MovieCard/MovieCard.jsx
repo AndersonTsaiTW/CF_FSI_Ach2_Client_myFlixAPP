1f3608699096c415b0db3f8148cf10eb0eeffb44
Anderson <113076298+AndersonTsaiTW@users.noreply.github.com>
2024-05-21 02:11:45 -0400

    Connect to the Movie Api and use propTypes to check props 1. Use useEFFECT to connect to the API I build before 2. Rename the variable from original data to decrease the unnecessary data layers 3.  Use prop-types to check props

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,8 +1,20 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movieData, onMovieClick }) => {
   return (
     <div onClick = {() => { 
       onMovieClick(movieData);
     }}
-    >{movieData.Title}</div>
+    >{movieData.title}</div>
   )
-}
+};
+
+MovieCard.propTypes = {
+  movieData: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
--- src/components/MovieView/MovieView.jsx
+++ src/components/MovieView/MovieView.jsx
@@ -1,17 +1,29 @@
+import PropTypes from "prop-types";
+
 export const MovieView = ({ movieData, onBackClick }) => {
-  console.log(movieData); // 在這裡印出 movieData
+  console.log(movieData); // print movie data
   return (
     <div>
       <div>
-        <img src={movieData.ImagePath} alt={movieData.Title} />
+        <img src={movieData.image} alt={movieData.title} />
       </div>
       <div>
-        <span>Title: {movieData.Title}</span>
+        <span>Title: {movieData.title}</span>
       </div>
       <div>
-        <span>Genre: {movieData.Genre.Name}</span>
+        <span>Genre: {movieData.genre}</span>
       </div>
       <button onClick = {onBackClick}>Back</button>
     </div>
   );
-}
+};
+
+MovieView.propTypes = {
+  movieData: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
+};
